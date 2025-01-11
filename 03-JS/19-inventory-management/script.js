be69f7bf0e0dffc1bfaa1676b5f8a56bfc2c391f
leontoys <svliyon@gmail.com>
2025-01-12 10:47:59 +1100

    inventory manage

---
+++ 03-JS/19-inventory-management/script.js
@@ -0,0 +1,41 @@
+let inventory = [];
+
+const findProductIndex = (productName) => {
+  for(let index = 0; index < inventory.length; index++){
+    if(inventory[index].name.toLowerCase() == productName.toLowerCase() ){
+      return index;
+    }    
+  }
+  return -1;
+}
+
+const addProduct = (product)=>{
+  let {name,quantity} = product;
+  name = name.toLowerCase();
+  let index = findProductIndex(name);
+  if(index == -1){
+    inventory.push({name:name,quantity:quantity});
+    console.log(`${name} added to inventory`);
+  }
+  else{
+    inventory[index].quantity += product.quantity;
+    console.log(`${name} quantity updated`);
+  }
+}
+
+const removeProduct = (name,quantity)=>{
+  name = name.toLowerCase();
+  let index = findProductIndex(name);
+  if(index == -1){
+    console.log(`${name} not found`);
+  }
+  else{
+    inventory[index].quantity -= quantity;
+    if(inventory[index].quantity == 0){
+      inventory.splice(index,1);
+    }
+    else{
+    console.log(`Remaining ${name} pieces: ${inventory[index].quantity}`);
+    }
+  }
+}
