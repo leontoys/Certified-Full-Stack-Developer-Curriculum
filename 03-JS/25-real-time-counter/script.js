f94eeb232584765fa9eea3ab86a33c07d944baaf
leontoys <svliyon@gmail.com>
2025-01-14 13:13:23 +1100

    real time character counter

--- 03-JS/25-real-time-counter/script.js
+++ 03-JS/25-real-time-counter/script.js
@@ -5,11 +5,16 @@ const charCount  = document.getElementById("char-count");
 
 textInput.addEventListener("input",(event)=>{
 
-  let count = event.target.value.length;
+  let count = event.target.value.length;  
 
-  if(count==50){
+  if(count>=50){
     charCount.style.color = 'red';
+    textInput.value = event.target.value.slice(0, 50);    
+  } 
+  else{
+    charCount.style.color = 'black';
   }
+   
   charCount.innerText = `Character Count: ${count}/50`;
 
 })
