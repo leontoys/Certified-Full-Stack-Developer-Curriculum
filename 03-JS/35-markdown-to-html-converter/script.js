4b03784616ccc6b9bb366fdd298fd53af01d7bdc
leontoys <svliyon@gmail.com>
2025-01-21 07:41:46 +1100

    music-player-version-0

--- 03-JS/35-markdown-to-html-converter/script.js
+++ 03-JS/35-markdown-to-html-converter/script.js
@@ -8,9 +8,10 @@ const preview = document.getElementById("preview")
 /**********************
  * Add Event Listeners
  **********************/
-markdownIput.addEventListener("change",()=>{
+markdownIput.addEventListener("input",()=>{
     const result = convertMarkdown()
-    htmlOutput.innerHTML = result
+    htmlOutput.innerText = result
+
 })
 
 /***********************
@@ -21,6 +22,9 @@ function convertMarkdown(){
     const regexh3 = /^[#]{3}\s/
     const regexh2 = /^[#]{2}\s/
     const regexh1 = /^[#]{1}\s/
+    let input = ''
+    let match = null
+    let output = ''
 
     if(markdownIput.value.match(regexh3)){
         console.log("h3",markdownIput.value.match(regexh3))
@@ -29,7 +33,10 @@ function convertMarkdown(){
         console.log("h2",markdownIput.value.match(regexh2))
     }
     else    if(markdownIput.value.match(regexh1)){
-        console.log("h1",markdownIput.value.match(regexh1))
+        match = markdownIput.value.match(regexh1)
+        input = markdownIput.value.slice(match["index"]+2)
+        output = `<h1>${input}</h1>`
+        return output
     } 
 }
 
