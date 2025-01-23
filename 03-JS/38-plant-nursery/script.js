545ee3eabffe3fcb15829b10b5cca3fa455d3648
leontoys <svliyon@gmail.com>
2025-01-24 07:47:48 +1100

    step14-in-progress

--- 03-JS/38-plant-nursery/script.js
+++ 03-JS/38-plant-nursery/script.js
@@ -35,13 +35,24 @@ const royalCrown = {
 }
 
 const catalog = new Map()
-catalog.set(ballerina, { small: 20, medium: 15, large: 12 })
-catalog.set(prettyPolly, { small: 20, medium: 15, large: 12 })
-catalog.set(willowVale, { small: 20, medium: 15, large: 12 })
-catalog.set(hidcote, { small: 20, medium: 15, large: 12 })
-catalog.set(imperialGem, { small: 20, medium: 15, large: 12 })
-catalog.set(royalCrown, { small: 20, medium: 15, large: 12 })
+catalog.set(ballerina, { small: 20, medium: 15, large: 12 });
+catalog.set(prettyPolly, { small: 31, medium: 14, large: 24 });
+catalog.set(willowVale, { small: 3, medium: 5, large: 0 });
+catalog.set(hidcote, { small: 33, medium: 13, large: 18 });
+catalog.set(imperialGem, { small: 19, medium: 35, large: 28 });
+catalog.set(royalCrown, { small: 40, medium: 22, large: 9 });
 
+const sellPlants = (plant,potSize,potNumbers)=>{
+    if(potNumbers > catalog.get(plant).potSize){
+        //return `Not enough ${potSize} size pots for ${plant.scientificName} '${plant.cultivar}'. Only ${catalog.get(plant)[potSize]} left`
+        return `Not enough ${potSize} size pots for ${plant.scientificName} '${plant.cultivar}'. Only ${catalog.get(plant)[potSize]} left.`
+    }
+    else{
+        catalog.get(plant)[potSize] -= potSize
+        return `Catalog successfully updated.`
+    }
+}
+console.log(sellPlants(ballerina,"small",25))
+console.log(sellPlants(ballerina,"small",10))
 
 console.log(catalog)
-console.log(catalog.size)
