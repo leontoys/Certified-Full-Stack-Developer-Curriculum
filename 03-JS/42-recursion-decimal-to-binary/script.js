a903bddaf2ff95abe9eeb979d55b7f8e57f376b1
leontoys <svliyon@gmail.com>
2025-01-26 12:42:01 +1100

    step18-completed

---
+++ 03-JS/42-recursion-decimal-to-binary/script.js
@@ -0,0 +1,24 @@
+const numberInput =  document.getElementById("number-input")
+const convertBtn = document.getElementById("convert-btn")
+const result = document.getElementById("result")
+
+const  decimalToBinary = (input)=>{
+    return "1"
+}
+
+const checkUserInput = ()=>{
+    if (!numberInput.value||isNaN(parseInt(numberInput.value))||parseInt(numberInput.value)<0) {
+        window.alert("Please provide a decimal number greater than or equal to 0") 
+        return       
+    }
+    decimalToBinary(parseInt(numberInput.value))
+    numberInput.value = ""
+}
+
+convertBtn.addEventListener("click",checkUserInput)
+
+numberInput.addEventListener("keydown",(e)=>{
+    if (e.key==="Enter") {
+        checkUserInput()
+    }
+})
