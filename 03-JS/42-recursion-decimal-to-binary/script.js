6fc0bf6bfa0e3cb3b0dbf5ecfcb7ca8d3eba98e4
leontoys <svliyon@gmail.com>
2025-01-28 07:29:52 +1100

    decimal-to-binary-recursion-complete

--- 03-JS/42-recursion-decimal-to-binary/script.js
+++ 03-JS/42-recursion-decimal-to-binary/script.js
@@ -47,15 +47,24 @@ const animationContainer = document.getElementById("animation-container")
 const animationData = [
 {
   inputVal : 5,//will represent the value of the input each time your recursive function runs
-  addElDelay : 1000//will be the delay between adding DOM elements to the page
+  addElDelay : 1000,//will be the delay between adding DOM elements to the page
+  msg : "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.", 
+  showMsgDelay : 15000,
+  removeElDelay : 20000 
 },
 {
   inputVal : 2,//will represent the value of the input each time your recursive function runs
-  addElDelay : 1500//will be the delay between adding DOM elements to the page
+  addElDelay : 1500,//will be the delay between adding DOM elements to the page
+  msg : "decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
+  showMsgDelay : 10000,
+  removeElDelay : 15000
 },
 {
-  inputVal : 1,//will represent the value of the input each time your recursive function runs
-  addElDelay : 2000//will be the delay between adding DOM elements to the page
+  inputVal: 1,
+  addElDelay: 2000,
+  msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+showMsgDelay : 5000,
+removeElDelay : 10000,    
 }
 ]
 
@@ -113,8 +122,21 @@ const showAnimation = ()=>{
 
   animationData.forEach(obj => {
     setTimeout(() => {
-      animationContainer.innerHTML += `<p id="${obj.inputVal}" class="animation-frame">${decimalToBinary()}</p>`
+      animationContainer.innerHTML += `<p id="${obj.inputVal}" class="animation-frame">decimalToBinary(${obj.inputVal})</p>`
     }, obj.addElDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg
+    }, obj.showMsgDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).remove()
+    }, obj.removeElDelay);
+
+    setTimeout(() => {
+      result.textContent = decimalToBinary(5)
+    }, 20000);
+
   });
 
 /*  setTimeout(() => {
