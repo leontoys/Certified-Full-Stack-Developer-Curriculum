866dccfbd5705d3c4ba068f634ac987691c43e0b
leontoys <svliyon@gmail.com>
2025-01-29 06:43:54 +1100

    js-dsa

--- 03-JS/45-leader-board/script.js
+++ 03-JS/45-leader-board/script.js
@@ -1,2 +1,14 @@
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json"
 const forumTopicUrl = "https://forum.freecodecamp.org/t/"
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/"
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp"
+const postsContainer = document.getElementById("posts-container")
+
+const fetchData = async ()=>{
+    try {
+        const res = await fetch(forumLatest)
+        const data = res.json()
+    } catch (err) {
+        
+    }
+}
