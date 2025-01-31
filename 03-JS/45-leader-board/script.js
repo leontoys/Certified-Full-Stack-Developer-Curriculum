c70a862007712ae1d6100576f9d6671443e56424
leontoys <svliyon@gmail.com>
2025-01-31 12:50:20 +1100

    leader-board-init

--- 03-JS/45-leader-board/script.js
+++ 03-JS/45-leader-board/script.js
@@ -1,14 +1,16 @@
-const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json"
-const forumTopicUrl = "https://forum.freecodecamp.org/t/"
-const forumCategoryUrl = "https://forum.freecodecamp.org/c/"
-const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp"
-const postsContainer = document.getElementById("posts-container")
+const forumLatest =
+  'https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json';
+const forumTopicUrl = 'https://forum.freecodecamp.org/t/';
+const forumCategoryUrl = 'https://forum.freecodecamp.org/c/';
+const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
 
-const fetchData = async ()=>{
-    try {
-        const res = await fetch(forumLatest)
-        const data = res.json()
-    } catch (err) {
-        
-    }
-}
+const allCategories = {
+  299: { category: 'Career Advice', className: 'career' },
+  409: { category: 'Project Feedback', className: 'feedback' },
+  417: { category: 'freeCodeCamp Support', className: 'support' },
+  421: { category: 'JavaScript', className: 'javascript' },
+  423: { category: 'HTML - CSS', className: 'html-css' },
+  424: { category: 'Python', className: 'python' },
+  432: { category: 'You Can Do This!', className: 'motivation' },
+  560: { category: 'Backend Development', className: 'backend' }
+};
