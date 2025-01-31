6aed5cb9a0849caeefc1f4ea52dd1439023f5510
leontoys <svliyon@gmail.com>
2025-01-31 13:55:14 +1100

    leader-board

--- 03-JS/45-leader-board/script.js
+++ 03-JS/45-leader-board/script.js
@@ -14,3 +14,57 @@ const allCategories = {
   432: { category: 'You Can Do This!', className: 'motivation' },
   560: { category: 'Backend Development', className: 'backend' }
 };
+
+const timeAgo = (timeISO)=>{//timestamp in the ISO 8601 format 
+    const now = new Date()
+    const time = new Date(timeISO)
+    //console.log("now",typeof now)
+    //console.log("time",typeof time)
+    const difference = now - time
+    //console.log("difference",difference)
+    const differenceMinutes = ( difference ) / ( 1000 * 60 )
+    const differenceHours = ( difference ) / ( 1000 * 60 * 60 )
+    const differenceDays = ( difference ) / ( 1000 * 60 * 60 * 24 )
+
+    if(differenceMinutes < 60){
+        console.log("difference---minutes",differenceMinutes)
+        return `${Math.floor(differenceMinutes)}m ago`
+    }
+    else if(differenceHours < 24){
+        console.log("difference---hours",differenceHours)
+        return `${Math.floor(differenceHours)}h ago`
+    }
+    else{
+        console.log("difference---days",differenceDays)
+        return `${Math.floor(differenceDays)}d ago`
+
+    }
+}
+
+const viewCount = (views)=>{
+    if(views >= 1000){
+        return `${Math.floor(views/1000)}k`
+    }
+    else{
+        return views
+    }
+}
+
+const forumCategory = (id)=>{
+    const entries = Object.keys(allCategories)
+    console.log("id",id)
+    const found = entries.find(entry=>entry===id)
+    console.log("found",found)
+    return found
+}
+
+/* const today = new Date("2025-01-31")
+console.log("today",today)
+timeAgo(today)
+const yesterday = new Date("2025-01-30")
+console.log("someday",yesterday)
+timeAgo(yesterday)  */
+//console.log(viewCount(597))
+//console.log(viewCount(1000))
+console.log(forumCategory(299))
+console.log(forumCategory(200))  
