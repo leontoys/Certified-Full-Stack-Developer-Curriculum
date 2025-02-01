aa685578231aa36e743aab70393785513bdcac81
leontoys <svliyon@gmail.com>
2025-02-01 14:51:52 +1100

    reusable-footer-done

--- 04-REACT/02-reusable-footer/index.jsx
+++ 04-REACT/02-reusable-footer/index.jsx
@@ -1,3 +1,22 @@
 export function Footer(){
-    return ()
+    return (
+        <footer>
+            <ul>
+                <li>Fitness Dashboard</li>
+                <li>Services</li>
+            </ul>
+            <ul>
+                <li>Watch Videos</li>
+                <li>Discord</li>
+            </ul>
+            <ul>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions</li>
+            </ul>
+            <p>© 2024 Fitness Dashboard. All Rights Reserved.</p> 
+            <a href="#">Facebook</a>
+            <a href="#">Amazon</a>
+            <a href="#">Netflix</a>                       
+        </footer>
+    )
 }
