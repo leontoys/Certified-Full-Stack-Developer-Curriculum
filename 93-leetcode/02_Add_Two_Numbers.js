fecad0a90af8d538e7bef702751fb187aa6d4a5b
leontoys <svliyon@gmail.com>
2025-02-07 14:05:49 +1100

    leetcode-3

--- 93-leetcode/02_Add_Two_Numbers.js
+++ 93-leetcode/02_Add_Two_Numbers.js
@@ -1,13 +1,15 @@
-var addTwoNumbers = function(l1, l2) {
-    //const l1_reversed = Number(l1.toReversed().join(""))
-    //const l2_reversed = Number(l2.toReversed().join(""))
-    //const l3 = l1_reversed + l2_reversed
-    //const l3_string = String(l3).split("").reverse()
-    //const l3_numbers = l3_string.map(element=>Number(element))
-    //return String(Number(l1.toReversed().join("")) + Number(l2.toReversed().join(""))).split("").reverse().map(element=>Number(element))
-    return String(Number(l1.reverse().join("")) + Number(l2.reverse().join(""))).split("").reverse().map(element=>Number(element))
-    //console.log(l1_reversed)
-    //console.log(l3_string)
-};
+// Definition of ListNode
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
 
-console.log(addTwoNumbers([2,4,3],[5,6,4]))
+// Constructing the first linked list: [2,4,3] (which represents 342 in reverse)
+let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+
+// Constructing the second linked list: [5,6,4] (which represents 465 in reverse)
+let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+// Printing the linked lists for verification
+console.log(l1);
+console.log(l2);  
--- 94-Coding-Interview-Prep/01_Symmetric_Difference.js
+++ 94-Coding-Interview-Prep/01_Symmetric_Difference.js
@@ -4,27 +4,19 @@ function sym(...args) {
   args.forEach((element) => {
     let duplicates_removed = [];
     //remove duplicates from the array
-    element.forEach((number) => {
-      duplicates_removed.includes(number)
-        ? false
-        : duplicates_removed.push(number);
-    });
+    element.forEach((number) => duplicates_removed.includes(number) ? false: duplicates_removed.push(number));
     //now check if the number is already added to result
-    duplicates_removed.forEach((number) => {
-      //console.log(number)
-      if (result.includes(number)) {
-        //console.log("yes");
-        //if added remove it from result
-        result = result.filter((member) => member !== number);
-      } else {
-        //otherwise add to result
-        //console.log("no");
-        result.push(number);
-      }
-    });
-  });
-  return result;
-}
+    duplicates_removed.forEach((number) => result.includes(number) ? result = result.filter((member) => member !== number) : result.push(number))
+  })
+    return result;
+} 
 
-//console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+/* function sym(...args) {
+  //using sets
+  let result = new Set()
+  args.forEach(element => result = result.symmetricDifference(new Set(element)) )
+  return Array.from( result )
+} */
+
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
 console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]));
