233f45ec3bf2be659b56c4ddd0a495608ee9d8c0
leontoys <svliyon@gmail.com>
2025-02-10 10:08:45 +1100

    selection-sort

---
+++ 94-Coding-Interview-Prep/06_Selection_Sort.js
@@ -0,0 +1,8 @@
+function selectionSort(array) {
+    // Only change code below this line
+    return array;
+    // Only change code above this line
+  }
+
+//selection sort
+console.log(selectionSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]))
