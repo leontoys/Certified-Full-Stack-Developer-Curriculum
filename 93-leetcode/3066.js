9746bd24b909a61625851bfc16066725662e499e
leontoys <svliyon@gmail.com>
2025-02-14 07:22:58 +1100

    binary-search-completed

--- 93-leetcode/3066.js
+++ 93-leetcode/3066.js
@@ -45,17 +45,27 @@
 } */
 
 
-    var minOperations = function(nums, k) {
+/* var minOperations = function(nums, k) {
         
     let operations = 0    
     while(nums.length >= 2){  
         //find the minimum
         let minIndex = 0
-        let maxIndex = 0//second smallest
-        for(let i=1;i < nums.length;i++){
-            if(nums[minIndex] > nums[i]){
-                minIndex = i
+        let maxIndex = 1//second smallest
+        if(nums[0] > nums[1]){
+            minIndex = 1
+            maxIndex = 0
+        }
+        for(let i=2;i < nums.length;i++){
+            //we need to find the smallest of these three
+            if(nums[i] < nums[minIndex])//minindex is the least so if i is less than that
+            {
+                maxIndex = minIndex//this becomes second small
+                minIndex = i//smallest
             }
+            else if(nums[i] < nums[maxIndex]){
+                maxIndex = i//second smallest
+            }        
         }
         //after this loop we have the minium
         let minimum = nums[minIndex]
@@ -64,16 +74,9 @@
         }
         //now remove this from 
         nums.splice(minIndex,1)
-        //now find the next smallest
-        minIndex = 0
-        for(let i=1;i < nums.length;i++){
-            if(nums[minIndex] > nums[i]){
-                minIndex = i
-            }
-        }
         //after this loop we have the minium
-        let maximum = nums[minIndex] 
-        nums.splice(minIndex,1)
+        let maximum = nums[maxIndex] 
+        nums.splice(maxIndex,1)
         let sum = (2*minimum)+maximum
         //add this number
         nums.unshift(sum)
@@ -83,6 +86,32 @@
     return operations
 
     };  
+ */
+
+
+var minOperations = function(nums, k) {
+//sort nums array
+nums.sort((a, b) => a - b);
+let b = []
+let operations = 0
+
+//now loop through this array
+for(let i = j= 0 ; i <10 ;  ){
+    //i < nums.length && nums[i] < k && nums.length >=2
+    //we have to find the largest among the two arrays
+    //the old array, the new array that has sum
+    if(nums.length+b.length < 2){
+        //if we have less than two elements
+        //jump off 
+        break
+    }
+
+    //find the smallest among the two arrays
+    
+
+}
+
+};
 
 console.log(minOperations([2,11,10,1,3],10))
 console.log(minOperations([1,1,2,4,9],20))
